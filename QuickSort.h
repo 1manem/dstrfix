77ea3ada9a094f99031cc999891a12dd0085716e
marchandashaza <133954133+marchandashaza@users.noreply.github.com>
2023-05-31 01:28:19 +0800

    baba

--- QuickSort.h
+++ QuickSort.h
@@ -1,97 +1,59 @@
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
 
-// struct Node {
-//     string data;
-//     struct Node* next;
-//     struct Node* prev;
-// };
+template <class Node>
+void swapNodes(Node* a, Node* b) {
+    if (a == nullptr || b == nullptr)
+        return;
 
-// void printList(struct Node* node)
-// {
-//     cout << "Sorted list:" << endl;
-//     while (node != NULL) {
-//         cout << node->data << endl;
-//         node = node->next;
-//     }
-// }
+    // Swap the next pointers
+    Node* temp = a->nextAdd;
+    a->nextAdd = b->nextAdd;
+    b->nextAdd = temp;
 
-// struct Node* getTail(struct Node* cur)
-// {
-//     while (cur != NULL && cur->next != NULL)
-//         cur = cur->next;
-//     return cur;
-// }
+    // Swap the prev pointers
+    temp = a->prevAdd;
+    a->prevAdd = b->prevAdd;
+    b->prevAdd = temp;
 
-// struct Node* partition(struct Node* head, struct Node* end,
-//                        struct Node** newHead,
-//                        struct Node** newEnd)
-// {
-//     struct Node* pivot = end;
-//     struct Node *prev = NULL, *cur = head, *tail = pivot;
+    // Update the next node's prev pointer
+    if (a->nextAdd != nullptr)
+        a->nextAdd->prevAdd = a;
+    if (b->nextAdd != nullptr)
+        b->nextAdd->prevAdd = b;
 
-//     while (cur != pivot) {
-//         if (cur->data < pivot->data) {
-//             if ((*newHead) == NULL)
-//                 (*newHead) = cur;
+    // Update the prev node's next pointer
+    if (a->prevAdd != nullptr)
+        a->prevAdd->nextAdd = a;
+    if (b->prevAdd != nullptr)
+        b->prevAdd->nextAdd = b;
+}
 
-//             prev = cur;
-//             cur = cur->next;
-//         }
-//         else {
-//             if (prev)
-//                 prev->next = cur->next;
-//             struct Node* tmp = cur->next;
-//             cur->next = NULL;
-//             cur->prev = NULL;
-//             tail->next = cur;
-//             cur->prev = tail;
-//             tail = cur;
-//             cur = tmp;
-//         }
-//     }
+template <class Node>
+Node* partition(Node* low, Node* high) {
+    double pivot = high->ScoreScaled;
+    Node* i = low->prevAdd;
 
-//     if ((*newHead) == NULL)
-//         (*newHead) = pivot;
+    for (Node* j = low; j != high; j = j->nextAdd) {
+        if (j->ScoreScaled <= pivot) {
+            i = (i == nullptr) ? low : i->nextAdd;
+            swapNodes(i, j);
+        }
+    }
 
-//     (*newEnd) = tail;
+    i = (i == nullptr) ? low : i->nextAdd;
+    swapNodes(i, high);
+    return i;
+}
 
-//     return pivot;
-// }
-
-// struct Node* quickSortRecur(struct Node* head, struct Node* end)
-// {
-//     if (!head || head == end)
-//         return head;
-
-//     Node* newHead = NULL, * newEnd = NULL;
-//     struct Node* pivot = partition(head, end, &newHead, &newEnd);
-
-//     if (newHead != pivot) {
-//         struct Node* tmp = newHead;
-//         while (tmp->next != pivot)
-//             tmp = tmp->next;
-//         tmp->next = NULL;
-
-//         newHead = quickSortRecur(newHead, tmp);
-
-//         tmp = getTail(newHead);
-//         tmp->next = pivot;
-//         pivot->prev = tmp;
-//     }
-
-//     pivot->next = quickSortRecur(pivot->next, newEnd);
-//     if (pivot->next != NULL)
-//         pivot->next->prev = pivot;
-
-//     return newHead;
-// }
-
-// void quickSort(struct Node** headRef)
-// {
-//     (*headRef) = quickSortRecur(*headRef, getTail(*headRef));
-//     return;
-// }
+template <class Node>
+void quickSort(univDLL.head, Node* low, Node* high) {
+    if (low != nullptr && high != nullptr && low != high && high->nextAdd != low) {
+        University* pivot = partition(low, high);
+        quickSort(univDLL, low, pivot->prevAdd);
+        quickSort(univDLL, pivot->nextAdd, high);
+    }
+};
