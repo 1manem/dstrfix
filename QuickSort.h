74dd5a1ce94e9dbcd855870d8ffed1e71d31d9de
1manem <tp062731@mail.apu.edu.my>
2023-05-31 08:01:26 +0800

    update

--- QuickSort.h
+++ QuickSort.h
@@ -1,12 +1,24 @@
+#include <iostream>
+using namespace std;
+
 template <class Node>
-Node* Partition(Node* low, Node* high) {
+Node* Partition(Node* low, Node* high, bool asc) {
         int pivot = high->rank;
         Node* i = low->prevAdd;
 
+
         for (Node* j = low; j != high; j = j->nextAdd) {
+            if (asc){
             if (j->rank >= pivot) {
                 i = (i == nullptr) ? low : i->nextAdd;
                 std::swap(i->rank, j->rank);
+            }
+            }
+            else{
+                if (j->rank <= pivot) {
+                i = (i == nullptr) ? low : i->nextAdd;
+                std::swap(i->rank, j->rank);
+            }
             }
         }
 
@@ -19,11 +31,11 @@ Node* Partition(Node* low, Node* high) {
 
     // Quicksort algorithm for linked list
     template <class Node>
-    void QuickSort(Node* low, Node* high) {
+    void QuickSort(Node* low, Node* high, bool asc) {
         if (high != nullptr && low != high && low != high->nextAdd) {
-            Node* pivot = Partition(low, high);
-            QuickSort(low, pivot->prevAdd);
-            QuickSort(pivot->nextAdd, high);
+            Node* pivot = Partition(low, high,asc);
+            QuickSort(low, pivot->prevAdd, asc);
+            QuickSort(pivot->nextAdd, high, asc);
         }
     }
     // template <class Node>
